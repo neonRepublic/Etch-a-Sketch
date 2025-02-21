af5ef075c9fcc16823aba092bf9b9466ed03611d
neonRepublic <194363997+neonRepublic@users.noreply.github.com>
2025-02-21 14:20:36 -0500

    updated  js

--- javascript.js
+++ javascript.js
@@ -15,3 +15,47 @@ divs.forEach(div => {
     });
 });
 
+
+document.querySelector('#resize').addEventListener('click', () => {
+    const resize = prompt("Enter new number of squares per side (max 100):")
+    if (resize <= 100) {
+        generateGrid(resize);
+    };
+});
+
+function generateGrid(size) {
+    const container = document.querySelector('.grid-container');
+    container.innerHTML = '';
+
+    const gridSize = 500;
+    const squareSize = gridSize / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement('div');
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = 'green';
+        });
+
+        container.appendChild(div);
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
